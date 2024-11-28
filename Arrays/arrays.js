059b04029921ba20bc20655ba72104b1bd7b8705
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-11-28 06:14:31 -0800

    Arrays- Done

---
+++ Arrays/arrays.js
@@ -0,0 +1,14 @@
+// way-1
+var superheros = [];
+var superpower = [];
+//way-2
+var superactor = [];
+superheros.push("spiderman");
+superpower.push(2);
+var allUsers = [];
+allUsers.push({ name: "", isActive: false });
+// Making arrays ot arrays 
+var MLModels = [
+    [255, 255, 255],
+    []
+];
---
+++ Arrays/arrays.ts
@@ -0,0 +1,24 @@
+// way-1
+const superheros: String[] = []
+const superpower: Number[] = []
+//way-2
+const superactor: Array<number> = []
+
+superheros.push("spiderman")
+superpower.push(2)
+
+
+// Array of Type
+type User = {
+    name: string,
+    isActive: boolean
+}
+ 
+const allUsers: User[] = []
+allUsers.push({name: "", isActive: false})
+
+// Making arrays ot arrays 
+const MLModels: number[][] = [
+    [255,255,255],
+    []
+]
