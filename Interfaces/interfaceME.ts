9384ea7ae77d93e15e7652c17691959306fc1c23
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-11-28 07:08:29 -0800

    Interface- Completed

--- Interfaces/interfaceME.ts
+++ Interfaces/interfaceME.ts
@@ -5,13 +5,24 @@ interface User {
     googleID?: string,
     // Unique thing
 
-    
+
     // Boring trick
     // startTrail: () => string
     // New Trick
     startTrail(): string,
     getCoupon(cName: string): number
 }
+
+// Also Called Reopeneing of interfaces
+// new thing can be added later also can be called from different classes an can be added direectly 
+interface User {
+    githubID: string
+}
+
+interface Admin extends User {
+    role: "admin" | "learner"
+}
+
 
 const h: User = {
     aid: 1122,
@@ -22,5 +33,21 @@ const h: User = {
     },
     getCoupon: (cName: "string") => {
         return 10
-    }
+    },
+    githubID:"12322"
 }
+
+const a: Admin = {
+    aid: 1122,
+    email: '',
+    role: "admin",
+    id: 1,
+    startTrail: () => {
+        return ""
+    },
+    getCoupon: (cName: "string") => {
+        return 10
+    },
+    githubID:"12322"
+}
+
