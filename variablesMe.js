9bde15c324df564fb5ae4708bd72453f507c4ffe
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-11-28 04:31:50 -0800

    Variables Done

--- variablesMe.js
+++ variablesMe.js
@@ -1,4 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/// String
 var greetings = "Hello , Krushna";
-var mynum = 6;
 greetings.toLowerCase();
 console.log(greetings);
+// number
+var userId = 334466;
+// boolean
+var isLoggedIn = false;
+// Function
+function getHero() {
+    return "man";
+}
+var hero;
+hero = getHero();
--- variablesMe.ts
+++ variablesMe.ts
@@ -1,5 +1,20 @@
-let greetings: string =  "Hello , Krushna";
-
-let mynum = 6
+/// String
+let greetings: string = "Hello , Krushna";
 greetings.toLowerCase()
 console.log(greetings);
+
+
+// number
+let userId = 334466
+
+// boolean
+let isLoggedIn: boolean = false
+
+// Function
+function getHero(){
+    return "man"
+}
+let hero: string
+hero = getHero();
+
+export {}
