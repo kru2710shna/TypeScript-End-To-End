5c9881da23ccdeb6334d76631918959024bb6f8f
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-11-28 20:45:02 -0800

    Genetics Complex Example

--- Genetics/genetics.js
+++ Genetics/genetics.js
@@ -14,3 +14,25 @@ function idetityFour(val) {
     return val;
 }
 idetityFour({ brand: "", type: 2 });
+function getSearchProducts(products) {
+    //do someting database operations 
+    var myIndex = 3;
+    return products[3];
+}
+var getMoreSearchProducts = function (products) {
+    //database operations
+    var myIndex = 4;
+    return products[myIndex];
+};
+// Complex Examples
+function anotherFunction(valOne, valTwo) {
+    return {
+        valOne: valOne,
+        valTwo: valTwo
+    };
+}
+anotherFunction(3, {
+    conection: "string",
+    username: "string",
+    password: "string"
+});
--- Genetics/genetics.ts
+++ Genetics/genetics.ts
@@ -25,3 +25,39 @@ interface Bottle {
 }
 
 idetityFour<Bottle>({ brand: "", type: 2 })
+
+
+function getSearchProducts<T>(products: T[]): T {
+    //do someting database operations 
+    const myIndex = 3
+    return products[3]
+}
+
+
+const getMoreSearchProducts = <T>(products: T[]): T => {
+    //database operations
+    const myIndex = 4
+    return products[myIndex]
+}
+
+
+interface Database {
+    conection: string
+    username: string
+    password: string
+}
+
+// Complex Examples
+
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object {
+    return {
+        valOne,
+        valTwo
+    }
+}
+
+anotherFunction(3, {
+    conection: "string",
+    username: "string",
+    password: "string"
+})
